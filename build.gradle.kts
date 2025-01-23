198e9c9603b1f1289517c5e78fec9f077972097b
Emily Xiong <xiongemi@gmail.com>
2025-01-23 00:33:29 -0500

    change the pipeline to .gradlew

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,10 @@
 plugins {
     // id("dev.nx.gradle.native") version("+")
 }
+
+develocity {
+    buildScan {
+        termsOfUseUrl.set("https://gradle.com/help/legal-terms-of-use")
+        termsOfUseAgree.set("yes")
+    }
+}
